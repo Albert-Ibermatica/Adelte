3c7499d7546a95c5389eb7ed73471dc17fd2a65b
Albert Gonzalez <a.gonzalez.diaz@ibermatica.com>
2022-11-21 17:43:28 +0100

    añadido pdf con la documentacion al repositorio

--- run_backend_linux.sh
+++ run_backend_linux.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-# acordarse de cambiar el username de la ruta que llama al python
-/home/pi/miniforge3/envs/backend-websocket/bin/python /home/pi/Documents/backend-websocket/app.py
+/home/pi/miniforge3/envs/backend-websocket/bin/python /home/pi/Desktop/backend-websocket/app.py
